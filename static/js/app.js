e832ec9d049499b5b93b98a9da0df89f5e658d00
ConnorHz <hertzconnor@gmail.com>
2020-12-27 16:14:05 -0600

    Completed table fill

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,31 @@
-// from data.js
+var debug = true;
 var tableData = data;
 
-// YOUR CODE HERE!
+function debugLog(message) {
+    if (debug) {
+        console.log(message);
+    }
+}
+
+function fillTable(data) {
+    debugLog("begin fill");
+
+    var tableBody = d3.select("#ufo-table-body");
+    tableBody.html("");
+
+    data.forEach(row => {
+        var newRow = tableBody.append("tr");
+        newRow.append("td").text(row.datetime || "");
+        newRow.append("td").text(row.city || "");
+        newRow.append("td").text(row.state || "");
+        newRow.append("td").text(row.country || "");
+        newRow.append("td").text(row.shape || "");
+        newRow.append("td").text(row.duration || "");
+        newRow.append("td").text(row.comments || "");
+    });
+
+
+    debugLog("end fill");
+}
+
+fillTable(tableData);
