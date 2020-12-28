2a34ebd889b24569a5d92a37fe181e623199710e
ConnorHz <hertzconnor@gmail.com>
2020-12-27 18:43:39 -0600

    Added additional filter options

--- static/js/app.js
+++ static/js/app.js
@@ -21,11 +21,23 @@ function formatDate(date) {
     return [year, month, day].join('-');
 }
 
+function titleCase(str) {
+    return str.toLowerCase().split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  }
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
 Date.prototype.addDays = function(days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
 }
+
+
 
 function fillTable(data) {
     debugLog("begin fill");
@@ -33,17 +45,24 @@ function fillTable(data) {
     var tableBody = d3.select("#ufo-table-body");
     tableBody.html("");
 
-    data.forEach(row => {
+    if (data.length > 0) {
+        data.forEach(row => {
+            var newRow = tableBody.append("tr");
+            newRow.append("td").text(row.datetime || "");
+            newRow.append("td").text(titleCase(row.city) || "");
+            newRow.append("td").text(row.state.toUpperCase() || "");
+            newRow.append("td").text(row.country.toUpperCase() || "");
+            newRow.append("td").text(row.shape || "");
+            newRow.append("td").text(row.durationMinutes || "");
+            newRow.append("td").text(row.comments || "");
+        });
+    } else {
         var newRow = tableBody.append("tr");
-        newRow.append("td").text(row.datetime || "");
-        newRow.append("td").text(row.city || "");
-        newRow.append("td").text(row.state || "");
-        newRow.append("td").text(row.country || "");
-        newRow.append("td").text(row.shape || "");
-        newRow.append("td").text(row.duration || "");
-        newRow.append("td").text(row.comments || "");
-    });
-
+        newRow.attr("class", "no-data")
+        var noData = newRow.append("td")
+        noData.attr("colspan", "7");
+        noData.text("No Data")
+    }
 
     debugLog("end fill");
 }
@@ -56,20 +75,49 @@ function filterTable() {
     var startDate = new Date(d3.select("#startDate").property("value"));
     var endDate = new Date(d3.select("#endDate").property("value")).addDays(1);
 
+    var cityFilter = document.getElementById('city-filter')
+    var city = cityFilter.options[cityFilter.selectedIndex].value;
+
+    var stateFilter = document.getElementById('state-filter')
+    var state = stateFilter.options[stateFilter.selectedIndex].value;
+
     debugLog(`Start Date: ${startDate}`);
     debugLog(`End Date: ${endDate}`);
-    
-    fillTable(tableData.filter(row => {
+    debugLog(`City: ${city}`);
+
+    var filteredData = tableData.filter(row => {
         var d = new Date(row.datetime);
-        return d >= startDate && d <= endDate;
-    }));
+        return (d >= startDate && d <= endDate) && (city == "default" | row.city == city) && (state == "default" | row.state == state);
+    });
+
+    fillTable(filteredData);
 
     debugLog("end filter");
 }
 
 var dates = tableData.map(row => {
-    return new Date(row.datetime)
-})
+    return new Date(row.datetime);
+});
+
+var cities = tableData.map(row => {
+    return titleCase(row.city);
+}).filter(onlyUnique).sort();
+
+cities.forEach(city => {
+    var newOption = d3.select("#city-filter").append("option");
+    newOption.text(city);
+    newOption.attr("value", city.toLowerCase());
+});
+
+var states = tableData.map(row => {
+    return row.state.toUpperCase();
+}).filter(onlyUnique).sort();
+
+states.forEach(state => {
+    var newOption = d3.select("#state-filter").append("option");
+    newOption.text(state);
+    newOption.attr("value", state.toLowerCase());
+});
 
 var minDate = formatDate(Math.min.apply(null,dates));
 var maxDate = formatDate(Math.max.apply(null,dates));
